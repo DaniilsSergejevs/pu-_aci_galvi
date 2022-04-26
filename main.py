ae2aba06d9c28b19985253de727904f24d60256f
Daniil Sergejev <77155713+DaniilsSergejevs@users.noreply.github.com>
2022-04-26 08:36:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a = int(input('Ievadi cik gadi ir puķīm: '))
+if a<=100:
+  b = a*3+3
+  c = b*2
+  print("Puķīm ir", b,"galvi un", c,"aci")
+else:
+  b = 100*3+3
+  b1 = a-100
+  b2 = b1*2
+  b3 = b+b2
+  c = b3*2
+  print("Puķīm ir", b3,"galvi un", c,"aci")
